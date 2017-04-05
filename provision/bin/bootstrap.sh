131b4074947757218d638079fadc32cb44536e00
Asfand Qazi <ayqazi@gmail.com>
2017-04-05 10:51:21 +0100

    Start setting up the VM proper

--- provision/bin/bootstrap.sh
+++ provision/bin/bootstrap.sh
@@ -7,12 +7,15 @@ user_uid() {
         echo "$(getent passwd ${user} | cut -d: -f3)"
 }
 
+cd "$(dirname ${0})/.."
+
 if [[ "$(id -un)" = vagrant && "$(user_uid vagrant)" = 1000 ]] ; then
         echo 'vagrant is uid 1000, script added to change this on reboot'
         echo 'Reboot machine to change this to you! (vagrant reload)'
         echo 'Continue provisioning (vagrant provision) after reboot'
-        sudo cp /vagrant/provision/bin/change-default-user.sh /etc/rc.local
+        sudo -H cp /vagrant/provision/bin/change-default-user.sh /etc/rc.local
 else
         echo 'Installing foundation packages, and provisioning'
-        sudo -H "$(dirname ${0})/install-foundation.sh"
+        sudo -H "bin/install-foundation.sh"
+        sudo -H "bin/full-provision.sh"
 fi
---
+++ provision/bin/full-provision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname ${0})/.."
+
+set -x
+
+ansible-playbook -i localhost, -c local -l localhost vm.yaml
