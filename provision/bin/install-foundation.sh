736432163b63f8480ba9104c11a0bb5b7ca96fac
Asfand Qazi <ayqazi@gmail.com>
2020-05-13 09:59:40 +0100

    Make compatible with Ubuntu focal

--- provision/bin/install-foundation.sh
+++ provision/bin/install-foundation.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-ANSIBLE_VERSION=2.6.11
+ANSIBLE_VERSION=2.8.12
 
 if ! which ansible &> /dev/null || \
                 [ "$(ansible --version | head -n1 | cut -d' ' -f2)" != "${ANSIBLE_VERSION}" ] ; then
         apt-get update
-        apt-get -y install libssl-dev libffi-dev python-pip
-        pip install ansible=="${ANSIBLE_VERSION}"
+        apt-get -y install libssl-dev libffi-dev python3-pip
+        pip3 install ansible=="${ANSIBLE_VERSION}"
 fi
